9c9fbb2c223bdd6f4a1c6d35ffa6244881dc0003
Sergio Veloza <velozasergio@gmail.com>
2018-02-26 10:46:12 -0500

    0.19
    Post/Crear - Etiquetas

--- resources/views/admin/posts/partials/form.blade.php
+++ resources/views/admin/posts/partials/form.blade.php
@@ -5,12 +5,39 @@
 	{{ Form::select('category_id', $categories, null, ['class' => 'form-control']) }}
 </div>
 <div class="form-group">
-	{{ Form::label('name', 'Nombre de la Categoria') }}
+	{{ Form::label('name', 'Nombre de la Entrada') }}
 	{{ Form::text('name',null, ['class' => 'form-control','id' => 'name'])}}
 </div>
 <div class="form-group">
 	{{ Form::label('slug', 'URL Amigable') }}
 	{{ Form::text('slug',null, ['class' => 'form-control','id' => 'slug'])}}
+</div>
+<div class="form-group">
+	{{ Form::label('file', 'Imagen') }}
+	{{ Form::file('file') }}
+</div>
+<div class="form-group">
+	{{ Form::label('status', 'Estado')}}
+	<label>
+		{{ Form::radio('status', 'PUBLISHED')}} Publicado
+	</label>
+	<label>
+		{{ Form::radio('status', 'DRAFT')}} Borrador
+	</label>
+</div>
+<div class="form-group">
+	{{ Form::label('tags','Etiquetas') }}
+	<div>
+		@foreach($tags as $tag)
+			<label>
+				{{ Form::checkbox('tags[]', $tag->id) }}{{ $tag->name }}
+			</label>
+		@endforeach
+	</div>
+</div>
+<div class="form-group">
+	{{ Form::label('excerpt', 'Extracto') }}
+	{{ Form::textarea('excerpt',null, ['class' => 'form-control', 'rows' => '2'])}}
 </div>
 <div class="form-group">
 	{{ Form::label('body', 'Descripcion') }}
